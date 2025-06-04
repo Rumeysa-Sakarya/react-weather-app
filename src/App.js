b0bc77bd786a9440801e7ec149ea8a3deeca539c
Rumeysa-sakarya <rumeysakarya.10@gmail.com>
2025-06-04 16:05:34 +0200

    Added GitHub repo url

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">weather app
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project is coded by{" "}
+        <a
+          href="https://www.shecodes.io/graduates/148456-rumeysa-sakarya"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Rumeysa Sakarya
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/Rumeysa-Sakarya/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+        .
+      </footer>
     </div>
   );
 }
