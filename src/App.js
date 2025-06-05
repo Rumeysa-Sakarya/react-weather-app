924c6c0eee488e6a53622ee02b163b039f69bfe6
Rumeysa-sakarya <rumeysakarya.10@gmail.com>
2025-06-05 18:10:38 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,26 +3,28 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is coded by{" "}
-        <a
-          href="https://www.shecodes.io/graduates/148456-rumeysa-sakarya"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Rumeysa Sakarya
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/Rumeysa-Sakarya/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-        .
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project is coded by{" "}
+          <a
+            href="https://www.shecodes.io/graduates/148456-rumeysa-sakarya"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Rumeysa Sakarya
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Rumeysa-Sakarya/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
